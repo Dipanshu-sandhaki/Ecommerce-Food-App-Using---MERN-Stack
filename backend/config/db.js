df90e52d3a8c9ddbebe27f017b55df25e94a7e85
Dipanshu Sandhaki <dipanshuroi6@gmail.com>
2025-06-07 01:54:08 +0530

    Delete unnecessary frontend files

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose"
-
-export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://dipanshu:988462@cluster0.8qqnlsd.mongodb.net/food-del').then(()=>console.log("Database connected"));
-}
